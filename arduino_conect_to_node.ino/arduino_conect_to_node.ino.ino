730daca43650fc81a5f705ba0cb8513635a3ce38
Frong-nt <60070025@kmitl.ac.th>
2018-04-22 18:12:55 +0700

    modified arduino conect to node

--- arduino_conect_to_node.ino/arduino_conect_to_node.ino.ino
+++ arduino_conect_to_node.ino/arduino_conect_to_node.ino.ino
@@ -2,14 +2,17 @@
 int mq7;
 float mq135;
 
-#include <SoftwareSerial.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h> 
 
+#include <SoftwareSerial.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 SoftwareSerial NanoSerial(11, 10); // RX | TX
 
 void setup()
 
 {  
-
+lcd.begin();
   pinMode(11,INPUT);
 
   pinMode(10,OUTPUT);
@@ -25,6 +28,15 @@ void loop() {
   mq135 = analogRead(A0);
   Serial.println(mq135);
   Serial.println(mq7);
+  
+  lcd.setCursor(0, 0);  
+  lcd.print("MQ-7   ");
+  lcd.print((float)mq7);
+  
+  lcd.setCursor(0, 1);
+  lcd.print("MQ-135 ");
+  lcd.print(mq135);
+
 
   NanoSerial.print(mq7);NanoSerial.print(" ");
   NanoSerial.print(mq135);
